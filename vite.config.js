c6ce0cac2ff89891f3edd018edba8350bee50110
sora <k.sora151201@gmail.com>
2023-11-05 17:05:28 +0900

    Add PWA assets and update manifest

--- vite.config.js
+++ vite.config.js
@@ -9,24 +9,28 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		VitePWA({
-			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "maskable-icon-512x512.png"],
 			manifest: {
-				name: "Vite App",
-				short_name: "Vite App",
+				name: "Tango",
+				short_name: "Tango",
+				description: "TangoChou",
 				theme_color: "#ffffff",
-				start_url: "/",
-				display: "standalone",
-				background_color: "#ffffff",
 				icons: [
 					{
-						src: "/icons/android-chrome-192x192.png",
+						src: "pwa-192x192.png",
 						sizes: "192x192",
 						type: "image/png",
 					},
 					{
-						src: "/icons/android-chrome-512x512.png",
+						src: "pwa-512x512.png",
 						sizes: "512x512",
 						type: "image/png",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
 					},
 				],
 			},
